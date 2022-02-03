2608924996ee718adf23f3dca640f2cf29853286
AdithyaDeSilva <adithyadesilva00@gmail.com>
2022-02-03 21:37:31 +0530

    Add all if else statements into a dictionary and redirect month number into month urls

--- challenges/views.py
+++ challenges/views.py
@@ -1,31 +1,55 @@
-from django.http import HttpResponse,HttpResponseNotFound
+from django.http import HttpResponse,HttpResponseNotFound,HttpResponseRedirect
 from django.shortcuts import render
+
+import challenges
+
+monthlyChallenges = {
+    "january": "Chicken dinners only!",
+    "february" : "Noobs only!",
+    "march": "GG bois!",
+    "april":"GGWP!",
+    "may": "Better luck next time!",
+    "june":"Ez for ence!",
+    "july": "This is dua lipa!",
+    "august":"New rules!",
+    "september": "Friends",
+    'october':"IDGAF",
+    "november":"Levitating",
+    "december":"No lie!"
+}
+
 
 # Create your views here.
 
 # def index(request):
 #     return HttpResponse('This works!')
-    
 # def january(request):
 #     return HttpResponse('Chicken dinners only!')
-
 # def february(request):
 #     return HttpResponse('Noobs only!')
-
 # def march(request):
 #     return HttpResponse('GG bois!')
 
+
 def monthlyChallengeByNumber(request, month):
-    return HttpResponse(month)
+    months = list(monthlyChallenges.keys()) #just using list fuynction for approve
+    redirectMonth = months[month - 1]
+    return HttpResponseRedirect("/challenges/"+redirectMonth)
 
 def monthlyChallenge(requset, month):
-    challengeText =None
-    if(month =='january'):
-        challengeText = "Chicken dinners only!"
-    elif (month == 'february'):
-        challengeText = 'Noobs only!'
-    elif (month == 'march'):
-        challengeText = "GG bois!"
-    else:
-        return HttpResponseNotFound("This month is not supported!")
-    return HttpResponse(challengeText)
+    # challengeText =None
+    # if(month =='january'):
+    #     challengeText = "Chicken dinners only!"
+    # elif (month == 'february'):
+    #     challengeText = 'Noobs only!'
+    # elif (month == 'march'):
+    #     challengeText = "GG bois!"
+    # else:
+    #     return HttpResponseNotFound("This month is not supported!")
+    # return HttpResponse(challengeText)
+
+    try:
+        challengeText = monthlyChallenges[month]
+        return HttpResponse(challengeText)
+    except:
+        return HttpResponseNotFound('This month is not eligible!')
