f6dfd4bc3cbe60e41cb4cf6a2de362b094e5ed45
AdithyaDeSilva <adithyadesilva00@gmail.com>
2022-02-04 16:32:57 +0530

    Update the ReadMe file

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 Just started to learn django.
+[Learnt with the academnd tutorial course]
+
+First section
+=============
+* URLs
+* Put in local urls into the project urls with include function 
+* Create view functions to be triggered when requested by the client
+* Created views for each month seperately
+* Replaced those function with a one function using if ladder
+* Replaced if ladder using a dictionary of months and challenges
+* Responded the request which made as number of the month
+* Redirect that request into the their urls   ex:- /challenges/1   =>  /challenges/january
+* Replace the hard coded redirecting path with dynamic reverse function
+* Send a HTML code as the response
+* Created the index view and responded the challenge/ url by returning a list of months which generates the links to the months with HTML
+
+
+
--- challenges/views.py
+++ challenges/views.py
@@ -2,8 +2,6 @@ from django.http import HttpResponse,HttpResponseNotFound,HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-import challenges
-
 monthlyChallenges = {
     "january": "Chicken dinners only!",
     "february" : "Noobs only!",
@@ -19,10 +17,6 @@ monthlyChallenges = {
     "december":"No lie!"
 }
 
-    
-
-
-
 # Create your views here.
 def index(request):
     listItem = ''
@@ -37,17 +31,6 @@ def index(request):
     responseData = f"<ul>{listItem}</ul>"
     return HttpResponse(responseData)
 
-
-# def index(request):
-#     return HttpResponse('This works!')
-# def january(request):
-#     return HttpResponse('Chicken dinners only!')
-# def february(request):
-#     return HttpResponse('Noobs only!')
-# def march(request):
-#     return HttpResponse('GG bois!')
-
-
 def monthlyChallengeByNumber(request, month):
     months = list(monthlyChallenges.keys()) #just using list fuynction for approve
 
@@ -59,17 +42,6 @@ def monthlyChallengeByNumber(request, month):
     return HttpResponseRedirect(redirectPath) # redirects into mentioned url  sends this url into the browser and browser send it back to the server
 
 def monthlyChallenge(requset, month):
-    # challengeText =None
-    # if(month =='january'):
-    #     challengeText = "Chicken dinners only!"
-    # elif (month == 'february'):
-    #     challengeText = 'Noobs only!'
-    # elif (month == 'march'):
-    #     challengeText = "GG bois!"
-    # else:
-    #     return HttpResponseNotFound("This month is not supported!")
-    # return HttpResponse(challengeText)
-
     try:
         challengeText = monthlyChallenges[month]
         resposeData = f"<h1>{challengeText}</h1>"
